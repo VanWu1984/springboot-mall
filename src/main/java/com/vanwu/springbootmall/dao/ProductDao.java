13621314c5f48569fa3f6343de6d3600d560979a
VanWu1984 <define1984@gmail.com>
2024-03-25 21:46:30 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- src/main/java/com/vanwu/springbootmall/dao/ProductDao.java
+++ src/main/java/com/vanwu/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.vanwu.springbootmall.dao;
 import com.vanwu.springbootmall.dto.ProductRequest;
 import com.vanwu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/vanwu/springbootmall/service/ProductService.java
+++ src/main/java/com/vanwu/springbootmall/service/ProductService.java
@@ -3,8 +3,11 @@ package com.vanwu.springbootmall.service;
 import com.vanwu.springbootmall.dto.ProductRequest;
 import com.vanwu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
